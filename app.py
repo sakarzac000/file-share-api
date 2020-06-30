7ed3aa1552924de1517962f307a6baf0d4729141
Zach Sakar <zach.sakar@gmail.com>
2020-06-30 11:03:32 -0600

    Added File table

--- app.py
+++ app.py
@@ -14,5 +14,24 @@ ma = Marshmallow
 heroku = Heroku(app)
 CORS(app)
 
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    file_type = db.Column(db.String(), nullable=False)
+    data = db.Column(db.LargeBinary, nullable=False)
+
+    def __init__(self, name, file_type, data):
+        self.name = name
+        self.file_type = file_type
+        self.data = data
+
+class FileSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "file_type")
+
+file_schema = FileSchema()
+files_schema = FileSchema(many=True)
+
 if __name__ == "__main__":
     app.run(debug=True)
